12331d54111ddc23ceda049b92b3899e56fa6d62
Henrik Danielsson <danielson.henrik@gmail.com>
2025-08-11 13:10:01 +0200

    Test log env vars in Azure

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,4 +1,8 @@
 // src/supabaseClient.ts
+console.log("SUPABASE_URL from env:", import.meta.env.VITE_SUPABASE_URL);
+console.log("SUPABASE_ANON_KEY from env:", import.meta.env.VITE_SUPABASE_ANON_KEY);
+
+
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
